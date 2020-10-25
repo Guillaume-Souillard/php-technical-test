c45439f20d74e85f68c54a9f07c85cdec2b9b6ed
Guillaume Souillard <guillaume.souillard@hotmail.fr>
2020-10-25 18:00:25 +0100

    env: install

--- install.sh
+++ install.sh
@@ -6,11 +6,13 @@ echo "La base de donnée a bien été créée."
 
 #Migrations
 echo "Lancement des migrations"
-php bin/console doctrine:migrations:migrate
+php bin/console doctrine:migrations:migrate -yes
 echo "Migrations OK"
 
 #Fixutes
 ## J'utilise ici les fixtures pour init des data de démoc
 echo "Utilisation des fixtures pour data de démo"
-php bin/console doctrine:fixtures:load
+php bin/console doctrine:fixtures:load -yes
 echo "fixtures OK"
+
+echo "Tu peux maintenant te rendre https://localhost et te connecter avec mail: stadline@stadline.com et pass: stadline"
