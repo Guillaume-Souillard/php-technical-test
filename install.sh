aae69dcaa04682fbee2545c6885bb0e874324ea6
Guillaume Souillard <guillaume.souillard@hotmail.fr>
2020-10-25 17:56:55 +0100

    env: install

--- install.sh
+++ install.sh
@@ -5,6 +5,12 @@ php bin/console doctrine:database:create
 echo "La base de donnée a bien été créée."
 
 #Migrations
+echo "Lancement des migrations"
+php bin/console doctrine:migrations:migrate
+echo "Migrations OK"
 
 #Fixutes
-## J'utilise ici les fixtures pour init des data de démo
+## J'utilise ici les fixtures pour init des data de démoc
+echo "Utilisation des fixtures pour data de démo"
+php bin/console doctrine:fixtures:load
+echo "fixtures OK"
